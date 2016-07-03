b93a73dd56d243d2a16ade944f9d8d92a87193c1
Naveen <its2hot@outlook.com>
2016-07-03 14:32:58 +0530

    Initial Commit

---
+++ UITableViewController Challenge Solution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Naveen Dangeti on 03/07/16.
+//  Copyright © 2016 Naveen Dangeti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableViewController Challenge Solution/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UITableViewController Challenge Solution
+//
+//  Created by Naveen Dangeti on 03/07/16.
+//  Copyright © 2016 Naveen Dangeti. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
