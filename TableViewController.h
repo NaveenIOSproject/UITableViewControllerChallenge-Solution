b02cd47717e5e385d5a48c8084d92866cef07482
Naveen <its2hot@outlook.com>
2016-07-05 00:12:34 +0530

    solved the challenge

--- TableViewController.h
+++ TableViewController.h
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  TableViewController.h
 //  UITableViewController Challenge Solution
 //
-//  Created by Naveen Dangeti on 03/07/16.
+//  Created by Naveen Dangeti on 04/07/16.
 //  Copyright © 2016 Naveen Dangeti. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface TableViewController : UITableViewController
 
 @end
-
